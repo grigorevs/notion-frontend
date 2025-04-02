6d71eba4246c284d0fbdeaa1e60d513a128ef41a
Semyon Grigorev <semyon@Mac-mini--Semyon.local>
2025-04-02 11:18:20 +0300

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,31 @@
-// .eslintrc.js
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier', // Заменили 'plugin:prettier/recommended' на 'prettier'
-      'plugin:react-hooks/recommended',
-    ],
-    plugins: ['prettier', 'react', 'react-hooks'],
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
-      },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:react-hooks/recommended',
+  ],
+  plugins: ['prettier', 'react', 'react-hooks'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'prettier/prettier': 'error',
-      'react/prop-types': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
     },
-    settings: {
-      react: {
-        pragma: 'React',
-        version: 'detect',
-      },
-    },
-  };
+  },
+};
